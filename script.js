15309b5d7ed6a4022530a90f68ec970540444ff5
Ivan <99640421+i4k1@users.noreply.github.com>
2023-12-13 20:54:00 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,75 @@
-if (typeof window.ethereum !== undefined && window.ethereum.isMetaMask) {
+if ( typeof window.ethereum !== undefined && window.ethereum.isMetaMask ) {
     console.log(`MetaMask is installed!`);
+
     const ethereumButton = document.querySelector(`.enableEthereumButton`);
     const showAccount = document.querySelector(`.showAccount`);
     const showChainID = document.querySelector(`.showChainID`);
 
     const changeChainIDButton = document.querySelector(`.changeChainIDButton`);
     let text_input_chainid = document.getElementById(`text_input_chainid`);
-    changeChainIDButton.addEventListener(`click`, async () => { await window.ethereum.request({ method: `wallet_switchEthereumChain`, params: [{ chainId: `0x${Number(text_input_chainid.value).toString(16)}` }] }); await getAccount(); });
+    changeChainIDButton.addEventListener(`click`, async () => {
+        await window.ethereum.request({
+                method: `wallet_switchEthereumChain`,
+                params: [{ chainId: `0x${Number(text_input_chainid.value).toString(16)}` }]
+        });
+        await getAccount();
+    });
 
     ethereumButton.addEventListener(`click`, () => { getAccount(); });
-    showAccount.innerHTML = `Connect <b>Wallet</b>`;
+    showAccount.innerHTML = `<b>Connect Wallet</b>`;
 
     async function getAccount() {
-        const accounts = await window.ethereum.request({ method: `eth_requestAccounts` }).catch((err) => { if (err.code === 4001) { console.log(`Please connect to MetaMask.`); } else { console.error(err); } });
+        const accounts = await window.ethereum.request({
+            method: `eth_requestAccounts`
+        }).catch((err) => {
+            if ( err.code === 4001 ) {
+                console.log(`Please connect to MetaMask.`);
+            } else {
+                console.error(err);
+            }
+        });
+
         const account = accounts[0];
-        showAccount.innerHTML = `<b>Account:</b> <code>${account}</code>`;
-        const chainId = await window.ethereum.request({ method: 'eth_chainId' });
-        showChainID.innerHTML = `<b>ChainID:</b> <code>${parseInt(chainId, 16)}</code>`;
+        showAccount.innerHTML = `<code>${account}</code>`;
+        // const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+        // showChainID.innerHTML = `<b>ChainID:</b> <code>${parseInt(chainId, 16)}</code>`;
     }
 
     const selectEthMainnet = document.querySelector(`.selectEthMainnet`);
-    selectEthMainnet.addEventListener(`click`, async () => { await window.ethereum.request({ method: `wallet_switchEthereumChain`, params: [{ chainId: `0x1` }] }); await getAccount(); });
+    selectEthMainnet.addEventListener(`click`, async () => {
+        await window.ethereum.request({
+            method: `wallet_switchEthereumChain`,
+            params: [{ chainId: `0x1` }]
+        });
+        await getAccount();
+    });
 
     const selectBnbTestnet = document.querySelector(`.selectBnbTestnet`);
-    selectBnbTestnet.addEventListener(`click`, async () => { await window.ethereum.request({ method: `wallet_switchEthereumChain`, params: [{ chainId: `0x61` }] }); await getAccount(); });
+    selectBnbTestnet.addEventListener(`click`, async () => {
+        await window.ethereum.request({
+            method: `wallet_switchEthereumChain`,
+            params: [{ chainId: `0x61` }]
+        });
+        await getAccount();
+    });
 
     const selectPolygonMumbai = document.querySelector(`.selectPolygonMumbai`);
-    selectPolygonMumbai.addEventListener(`click`, async () => { await window.ethereum.request({ method: `wallet_switchEthereumChain`, params: [{ chainId: `0x13881` }] }); await getAccount(); });
+    selectPolygonMumbai.addEventListener(`click`, async () => {
+        await window.ethereum.request({
+            method: `wallet_switchEthereumChain`,
+            params: [{ chainId: `0x13881` }]
+        });
+        await getAccount();
+    });
+
+    const selectArbitrumGoerli = document.querySelector(`.selectArbitrumGoerli`);
+    selectArbitrumGoerli.addEventListener(`click`, async () => {
+        await window.ethereum.request({
+            method: `wallet_switchEthereumChain`,
+            params: [{ chainId: `0x66eed` }]
+        });
+        await getAccount();
+    });
 } else {
     console.log(`MetaMask not installed!`);
 }
